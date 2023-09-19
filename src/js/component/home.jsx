5ea84c932e9af1b333d8559e2221664b14ef84a4
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-19 00:12:15 +0000

    making the todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const To_doList = () => {
+	
+	const [taskList, setTaskList] = useState(["mob the floor", "dish the washes", "Clean the house", "vacumm the carpet", "wash the dog"]);
+	const [inputValue, setInputValue] = useState("")
 
-//create your first component
-const Home = () => {
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (inputValue != "") {
+			setTaskList([...taskList, inputValue]);
+			setInputValue("");
+		}
+	};
+
+	const deleteTask = (i) => {
+		setTaskList(taskList.filter((_, index) => {
+			return index != i
+		}))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column">
+			<div className="text-center">
+				<h1><strong>ToDo's List Lex Brunett</strong></h1>
+			</div>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={(e) => { setInputValue(e.target.value) }}
+					value={inputValue}
+					type="text"
+					placeholder="Any tasks at the moment, add a task"
+				/>
+				<ul className="list-group">
+					{taskList.map((task, index) => (
+						<li key={index} className="list-group-item d-flex justify-content-between">
+							<span>
+								{task}
+							</span>
+							<button
+								type="button"
+								className="btn-close"
+								onClick={() => {deleteTask(index)}
+								}>
+							</button>
+						</li>
+					))}
+				</ul>
+			</form>
 		</div>
 	);
 };
 
-export default Home;
+export default To_doList;
